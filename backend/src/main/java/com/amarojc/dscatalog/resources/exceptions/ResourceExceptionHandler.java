037f933edf9f039d621b2331755c9653ad4d97cb
Jorge Amaro <inf.amaro.jc@gmail.com>
2023-03-17 16:27:49 -0300

    Exception Empty and Integrity database

--- backend/src/main/java/com/amarojc/dscatalog/resources/exceptions/ResourceExceptionHandler.java
+++ backend/src/main/java/com/amarojc/dscatalog/resources/exceptions/ResourceExceptionHandler.java
@@ -9,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.amarojc.dscatalog.services.exceptions.DatabaseIntegrityViolationException;
 import com.amarojc.dscatalog.services.exceptions.ObjectNotFoundException;
 
 
@@ -19,13 +20,30 @@ public class ResourceExceptionHandler {
 	public ResponseEntity<StandardError> entityNotFound(
 				ObjectNotFoundException e,HttpServletRequest request) {
 
+		HttpStatus status = HttpStatus.NOT_FOUND;
+		
 		StandardError error = new StandardError();
 		error.setTimestamp(Instant.now());
-		error.setStatus(HttpStatus.NOT_FOUND.value());
+		error.setStatus(status.value());
 		error.setError("Resource not found");
 		error.setMessage(e.getMessage());
 		error.setPath(request.getRequestURI());
 		
-		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
+		return ResponseEntity.status(status).body(error);
+	}
+	
+	@ExceptionHandler(DatabaseIntegrityViolationException.class)
+	public ResponseEntity<StandardError> databaseIntegrityViolation(
+			DatabaseIntegrityViolationException dex, HttpServletRequest request){
+		
+			HttpStatus status = HttpStatus.BAD_REQUEST;
+			StandardError error = new StandardError();
+			error.setTimestamp(Instant.now());
+			error.setStatus(status.value());
+			error.setError("Database Integrity violation");
+			error.setMessage(dex.getMessage());
+			error.setPath(request.getRequestURI());
+			
+			return ResponseEntity.status(status).body(error);
 	}
 }
