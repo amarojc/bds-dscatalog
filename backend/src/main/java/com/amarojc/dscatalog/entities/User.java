500fe10c745df7be4710d8e5f7fc764e790597d9
Jorge Amaro <inf.amaro.jc@gmail.com>
2023-06-16 00:04:12 -0300

    Implementing OAuth2 authorization server

--- backend/src/main/java/com/amarojc/dscatalog/entities/User.java
+++ backend/src/main/java/com/amarojc/dscatalog/entities/User.java
@@ -2,8 +2,10 @@ package com.amarojc.dscatalog.entities;
 
 import java.io.Serializable;
 import java.time.Instant;
+import java.util.Collection;
 import java.util.HashSet;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -18,9 +20,13 @@ import javax.persistence.PrePersist;
 import javax.persistence.PreUpdate;
 import javax.persistence.Table;
 
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
 @Entity
 @Table(name = "tb_user")
-public class User  implements Serializable{
+public class User  implements UserDetails, Serializable{
 	
 	private static final long serialVersionUID = 1L;
 	
@@ -142,5 +148,43 @@ public class User  implements Serializable{
 	@PreUpdate
 	public void preUpdatePersist() {
 		updateAt = Instant.now();
+	}
+
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		return roles.stream()
+				.map(role -> new SimpleGrantedAuthority(role.getAuthority()))
+				.collect(Collectors.toList());
+	}
+
+
+	@Override
+	public String getUsername() {
+		return email;
+	}
+
+
+	@Override
+	public boolean isAccountNonExpired() {
+		return true;
+	}
+
+
+	@Override
+	public boolean isAccountNonLocked() {
+		return true;
+	}
+
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		return true;
+	}
+
+
+	@Override
+	public boolean isEnabled() {
+		return true;
 	}
 }
